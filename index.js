d748f131b02cf9ed41604bd4c3e3e6a846a4a96d
Colleen <colleenhiggins87@gmail.com>
2019-06-11 12:07:37 -0700

    part two of assignment completed, up to 50 random dogs

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
 'use strict';
 
-
 function randomDogImage(urlNum){
 
-        fetch(`https://dog.ceo/api/breeds/image/random/${urlNum}`)
-        .then(response => response.json())
-        .then(responseJson => 
-          displayResults(responseJson))
-        .catch(error => alert('Something went wrong. Try again later.'));
-    }
+  fetch(`https://dog.ceo/api/breeds/image/random/${urlNum}`)
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson, urlNum))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
 
+function displayResults(responseJson, urlNum){
+    
+  let word = `Check out this ${urlNum} dog`;
+  if (urlNum > 1){
+    word = `Check out these ${urlNum} dogs`;
+  }
 
-    function displayResults(responseJson){
-        console.log(responseJson);
+  $('.results-desc').html(word);
 
-        responseJson.forEach(function)
+  responseJson.message.forEach(function(dogPic) 
+  {$('.results').append(`<img src='${dogPic}' class='results-img'>`);
+    console.log(dogPic);
+  });
+  $('.results').removeClass('hidden');
 
-    }
+}
 
 function userSubmit(){
-    $('form').submit((event=>{
-        event.preventDefault(event);
-        const userInput = $('.js-dogNumber').val();
-       const urlNum = userInput.toString();
-       randomDogImage(urlNum);
+  
+  $('form').submit((event=>{
+    event.preventDefault(event);
+    $('.results-img').remove();
+    const userInput = $('.js-dogNumber').val();
+    const urlNum = userInput.toString();
+    randomDogImage(urlNum);
+    
         
-    }))
+  }));
 }
 
 
 
 $(function (){
-    console.log()
-    userSubmit();
-})
+  console.log('works');
+  userSubmit();
+});
